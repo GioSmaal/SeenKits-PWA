e232fe3e0518ae12d007437ad7db82ac0a4869d1
i432855 <432855@student.fontys.nl>
2022-10-29 13:56:42 +0200

    SeenKits

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPWA = require('next-pwa');
 
-module.exports = nextConfig
+module.exports = withPWA({
+  reactStrictmode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+  },
+});
+
+
+// webpack.config.js:
+const { GenerateSW } = require('workbox-webpack-plugin');
+
+module.exports = {
+  // Other webpack configs...
+  plugins: [
+    // Other plugins...
+    new GenerateSW({
+      // option: 'value',
+    })
+  ]
+};
---
+++ public/sw.js
@@ -0,0 +1,48 @@
+// we'll version our cache (and learn how to delete caches in
+// some other post)
+const cacheName = 'v1::static';
+
+self.addEventListener('install', e => {
+  // once the SW is installed, go ahead and fetch the resources
+  // to make this work offline
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        /*
+          DEAR READER,
+          ADD A LIST OF YOUR ASSETS THAT
+          YOU WANT TO WORK WHEN OFFLINE
+          TO THIS ARRAY OF URLS
+        */
+      ]).then(() => self.skipWaiting());
+    })
+  );
+});
+
+// when the browser fetches a url, either response with
+// the cached object or go ahead and fetch the actual url
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // ensure we check the *right* cache to match against
+    caches.open(cacheName).then(cache => {
+      return cache.match(event.request).then(res => {
+        return res || fetch(event.request)
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(async function() {
+       try{
+         var res = await fetch(event.request);
+         var cache = await caches.open('cache');
+         cache.put(event.request.url, res.clone());
+         return res;
+       }
+       catch(error){
+         return caches.match(event.request);
+        }
+      }());
+  });
